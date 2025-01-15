0cb9ddc57ec07a86577a342359f2150231f35727
Bruno Manuel Santos Saraiva <bruno.msaraiva2@gmail.com>
2025-01-15 14:18:01 +0000

    fixing missing namechanges

--- src/maicroscopy_sandbox/__init__.py
+++ src/maicroscopy_sandbox/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Bruno Saraiva"""
 __email__ = "bruno.msaraiva2@gmail.com"
 __version__ = "0.1.0"
 
-from .maicroscopy_sandbox import mAIcroscopysandbox
+from .maicroscopy_sandbox import mAIcroscopySandbox
 from .samples.sample import Sample
 from .samples.ellipsoid import Ellipsoid
